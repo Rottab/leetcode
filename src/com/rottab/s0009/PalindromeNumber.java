c452406ceb9ec73a9e43d3b21cdddaf05fe770e6
Rottab <mr.rottab@gmail.com>
2022-04-02 19:12:49 +0200

    [main]: feat: 0009

---
+++ src/com/rottab/s0009/PalindromeNumber.java
@@ -0,0 +1,29 @@
+package com.rottab.s0009;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Math.abs;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        int current = x;
+        int reverse = 0;
+        int remainder = 0;
+        while (current != 0) {
+            remainder = current % 10;
+            reverse = reverse * 10 + remainder;
+            current = current / 10;
+        }
+        return abs(reverse) == x;
+    }
+
+
+    @Test
+    public void test() {
+        assert isPalindrome(121) == true;
+        assert isPalindrome(-121) == false;
+        assert isPalindrome(1231) == false;
+        assert isPalindrome(1221) == true;
+    }
+}
