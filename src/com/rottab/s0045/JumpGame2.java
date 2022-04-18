7880e75cd64bd56a1d18cb8d49eda57a35acea4f
Rottab <mr.rottab@gmail.com>
2022-04-18 19:20:48 +0200

    [main]: feat: improved

--- src/com/rottab/s0045/JumpGame2.java
+++ src/com/rottab/s0045/JumpGame2.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class JumpGame2 {
     // DP solution, not optimal cause it's n^2
-    public int jump(int[] nums) {
+    public int jump1(int[] nums) {
         int[] dp = new int[nums.length];
         dp[0] = 0;
         for (int i = 1; i < nums.length; i++) {
@@ -19,10 +19,34 @@ public class JumpGame2 {
     }
 
     @Test
-    public void test() {
-        assertThat(jump(new int[]{2, 3, 1, 1, 4})).isEqualTo(2);
-        assertThat(jump(new int[]{2, 3, 0, 1, 4})).isEqualTo(2);
-        assertThat(jump(new int[]{1, 2})).isEqualTo(1);
+    public void test1() {
+        assertThat(jump1(new int[]{2, 3, 1, 1, 4})).isEqualTo(2);
+        assertThat(jump1(new int[]{2, 3, 0, 1, 4})).isEqualTo(2);
+        assertThat(jump1(new int[]{1, 2})).isEqualTo(1);
+    }
+
+    // Greedy solution
+    public int jump2(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+        while (right < nums.length - 1) {
+            int max = 0;
+            for (int i = left; i < right + 1; i++) {
+                max = Integer.max(max, i + nums[i]);
+            }
+            left = right;
+            right = max;
+            jumps++;
+        }
+        return jumps;
+    }
+
+    @Test
+    public void test2() {
+        assertThat(jump2(new int[]{2, 3, 1, 1, 4})).isEqualTo(2);
+        assertThat(jump2(new int[]{2, 3, 0, 1, 4})).isEqualTo(2);
+        assertThat(jump2(new int[]{1, 2})).isEqualTo(1);
     }
 
 }
