0d485011eced19599276bb459a94940edc29cb12
Rottab <mr.rottab@gmail.com>
2022-04-22 03:05:19 +0200

    [main]: feat: solved

---
+++ src/com/rottab/s0104/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,38 @@
+package com.rottab.s0104;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Integer.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    @Test
+    public void test() {
+        assertThat(maxDepth(new TreeNode(3, new TreeNode(9, null, null),
+                new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)))))
+                .isEqualTo(3);
+    }
+}
