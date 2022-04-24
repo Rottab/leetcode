71fcedec8d7b6e33c6452484f12721ed7ecedebb
Rottab <mr.rottab@gmail.com>
2022-04-24 22:06:46 +0200

    [main]: feat: solved

---
+++ src/com/rottab/s0108/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,39 @@
+package com.rottab.s0108;
+
+import org.junit.jupiter.api.Test;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (right - left) / 2 + left;
+        return new TreeNode(nums[mid], sortedArrayToBST(nums, left, mid - 1), sortedArrayToBST(nums, mid + 1, right));
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    @Test
+    public void test() {
+        sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+    }
+}
