28e6ecf2e1c02e68a82f0b4444f06097550397e5
Rottab <mr.rottab@gmail.com>
2022-04-25 00:18:48 +0200

    [main]: feat: improved

--- src/com/rottab/s0111/MinimumDepthOFBinaryTree.java
+++ src/com/rottab/s0111/MinimumDepthOFBinaryTree.java
@@ -2,6 +2,10 @@ package com.rottab.s0111;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class TreeNode {
@@ -25,29 +29,62 @@ class TreeNode {
 
 public class MinimumDepthOFBinaryTree {
 
-    public int minDepth(TreeNode root, int depth) {
+    public int minDepth1(TreeNode root, int depth) {
         if (root.left == null && root.right == null) return depth + 1;
-        else if (root.left == null) return minDepth(root.right, depth + 1);
-        else if (root.right == null) return minDepth(root.left, depth + 1);
-        return Integer.min(minDepth(root.left, depth + 1), minDepth(root.right, depth + 1));
+        else if (root.left == null) return minDepth1(root.right, depth + 1);
+        else if (root.right == null) return minDepth1(root.left, depth + 1);
+        return Integer.min(minDepth1(root.left, depth + 1), minDepth1(root.right, depth + 1));
     }
 
-    public int minDepth(TreeNode root) {
+    public int minDepth1(TreeNode root) {
         if (root == null) return 0;
-        return minDepth(root, 0);
+        return minDepth1(root, 0);
     }
 
     @Test
-    public void test() {
-        assertThat(minDepth(
+    public void test1() {
+        assertThat(minDepth1(
                 new TreeNode(3, new TreeNode(9, null, null),
                         new TreeNode(20, new TreeNode(15, null, null),
                                 new TreeNode(7, null, null)))
         )).isEqualTo(2);
-        assertThat(minDepth(
+        assertThat(minDepth1(
                 new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3, null, null)))
         )).isEqualTo(3);
 
-        assertThat(minDepth(null)).isEqualTo(0);
+        assertThat(minDepth1(null)).isEqualTo(0);
+    }
+
+
+    public int minDepth2(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            List<TreeNode> level = new LinkedList<>();
+            while (!queue.isEmpty()) level.add(queue.remove());
+            for (TreeNode treeNode : level) {
+                if (treeNode.left == null && treeNode.right == null) return ++depth;
+                if (treeNode.left != null) queue.add(treeNode.left);
+                if (treeNode.right != null) queue.add(treeNode.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    @Test
+    public void test2() {
+        assertThat(minDepth2(
+                new TreeNode(3, new TreeNode(9, null, null),
+                        new TreeNode(20, new TreeNode(15, null, null),
+                                new TreeNode(7, null, null)))
+        )).isEqualTo(2);
+        assertThat(minDepth2(
+                new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3, null, null)))
+        )).isEqualTo(3);
+
+        assertThat(minDepth2(null)).isEqualTo(0);
     }
 }
