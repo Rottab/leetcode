076b6fc15fc0fe202989a225560a6876e6feda58
Rottab <mr.rottab@gmail.com>
2022-04-25 04:37:34 +0200

    [main]: feat: removed global variable

--- src/com/rottab/s0037/SudokuSolver.java
+++ src/com/rottab/s0037/SudokuSolver.java
@@ -6,9 +6,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SudokuSolver {
 
-    public void display(char[][] board){
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board.length; j++){
+    public void display(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + "\t");
             }
             System.out.println();
@@ -27,14 +27,11 @@ public class SudokuSolver {
         for (int i = 0; i < board.length; i++) {
             if (board[row][i] == value || board[i][column] == value) return false;
         }
-
-
         return true;
     }
 
-    public static boolean solved = false;
 
-    public void solve(char[][] board) {
+    public boolean solve(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (board[i][j] == '.') {
@@ -42,23 +39,21 @@ public class SudokuSolver {
                         char value = Character.forDigit(v, 10);
                         if (isValidate(board, i, j, value)) {
                             board[i][j] = value;
-                            solve(board);
-                            if (solved) return;
+                            if (solve(board)) return true;
                             board[i][j] = '.';
                         }
                     }
                     display(board);
-                    return;
+                    return false;
                 }
             }
         }
         System.out.println("SOLVED");
         display(board);
-        solved = true;
+        return true;
     }
 
-    public void solveSudoku(char[][] board){
-        solved = false;
+    public void solveSudoku(char[][] board) {
         solve(board);
     }
 
