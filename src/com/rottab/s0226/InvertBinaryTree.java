244d6d5a321b817b467e7d402c366f35562fa328
Rottab <mr.rottab@gmail.com>
2022-04-28 10:49:41 +0200

    [main]: feat: solved

---
+++ src/com/rottab/s0226/InvertBinaryTree.java
@@ -0,0 +1,37 @@
+package com.rottab.s0226;
+
+import org.junit.jupiter.api.Test;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = invertTree(temp);
+        return root;
+    }
+
+    @Test
+    public void test() {
+        invertTree(new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)));
+    }
+}
